2d4574d9d442f02049ca960ce3a2003007f86ca7
fernandezchris23 <30755152+fernandezchris23@users.noreply.github.com>
2017-08-21 21:27:37 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+for x in range(10): #range func makes it easy to make loops
+    print(x)
+print("\n")
+for x in range(5, 12): 
+    print(x)
+print("\n")
+for x in range(0, 10, 2):
+    print(x)
+print("\n")
+
+x = 5
+
+print("While Loop Section")
+while x < 10:
+    print(x)
+    x += 1
